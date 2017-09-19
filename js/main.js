fc352500768f3f6acbaa87cc672eb38a02dd67db
Humphrey Obuobi <obuobhum@gmail.com>
2017-09-19 06:57:54 -0400

    Firebase setup.

--- js/main.js
+++ js/main.js
@@ -1,52 +1,7 @@
-/*
-	Prism by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+var database = firebase.database();
+
+/* 
+When we start generating data from events, we'll want to work on writing information to the database.
+We should only be WRITING information to the database -- no need to retrieve data right now on the application, since we can do that via the console. 
+More information on https://firebase.google.com/docs/database/web/read-and-write
 */
-
-// (function($) {
-
-// 	skel.breakpoints({
-// 		xlarge:	'(max-width: 1680px)',
-// 		large:	'(max-width: 1280px)',
-// 		medium:	'(max-width: 980px)',
-// 		small:	'(max-width: 736px)',
-// 		xsmall:	'(max-width: 480px)'
-// 	});
-
-// 	$(function() {
-
-// 		var	$window = $(window),
-// 			$body = $('body');
-
-// 		// Disable animations/transitions until the page has loaded.
-// 			$body.addClass('is-loading');
-
-// 			$window.on('load', function() {
-// 				window.setTimeout(function() {
-// 					$body.removeClass('is-loading');
-// 				}, 100);
-// 			});
-
-// 		// Fix: Placeholder polyfill.
-// 			$('form').placeholder();
-
-// 		// Prioritize "important" elements on medium.
-// 			skel.on('+medium -medium', function() {
-// 				$.prioritize(
-// 					'.important\\28 medium\\29',
-// 					skel.breakpoint('medium').active
-// 				);
-// 			});
-
-// 	});
-
-// })(jQuery);
-
-window.onload = choosePic;
-
-var myPix = new Array("images/H.City.jpg", "images/M.City.jpg", "images/L.City.jpg");
-
-function choosePic() {
-	var randomNum = Math.floor(Math.random() * myPix.length);
-	document.getElementById("myPicture").src = myPix[randomNum];
