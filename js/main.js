d4563ba509d3f8c61981f6e9020406883870296a
Humphrey Obuobi <obuobhum@gmail.com>
2017-09-19 21:32:37 -0400

    code cleanup + interface revamp

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-// <<<<<<< HEAD
 function randomImg(){
     var randomNumber = Math.floor(Math.random() * 11) + 1;
     var imgName = randomNumber + ".png";
-    document.getElementById("imageid").src= "images" + "/" + imgName ;
+    document.getElementById("face").src= "./images/" + imgName ;
 
-    var randomNumber2 = Math.floor(Math.random() * 3) + 1;
+    var randomNumber2 = Math.ceil(Math.random() * 3);
     var imgName2 = "City" + randomNumber2 + ".jpg";
-    document.getElementById("backgroundid").src= "images" + "/" + imgName2 ;
+    document.getElementById("bg").src= "./images/" + imgName2 ;
  }
-// =======
 var database = firebase.database();
 
 /* 
@@ -16,4 +14,3 @@ When we start generating data from events, we'll want to work on writing informa
 We should only be WRITING information to the database -- no need to retrieve data right now on the application, since we can do that via the console. 
 More information on https://firebase.google.com/docs/database/web/read-and-write
 */
-// >>>>>>> origin/master
