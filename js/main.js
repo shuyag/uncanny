c81b0c35bb35bb3651cd23dfbde1ccd14ef4c275
Humphrey Obuobi <obuobhum@gmail.com>
2017-09-20 00:29:26 -0400

    timer functionality for response and movement times

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+var timer =
+{
+	'start': 0, 
+	'responseTime': 0,
+	'movementTime': 0, 
+	'stage1': false,
+	'stage2': false
+}
+//var database = firebase.database();
+var d = new Date();
+
+$('document').ready(function(){
+d
+	$('#next').click(function(){
+		timer.stage1 = false, timer.stage2 = false;
+		timer.start = 0, timer.responseTime = 0, timer.movementTime = 0; 
+		timer.stage1 = true;
+		timer.start = Date.now(); console.log(timer.start);
+	});
+	$("#next").mouseleave(function(){
+		if(!timer.stage1){
+			timer.stage1 = true;
+			timer.responseTime = Date.now() - timer.start; console.log(timer.responseTime)
+		}
+	});
+	$('.btn').mouseenter(function(){
+		if(!timer.stage2){
+			timer.stage2 = true;
+			timer.movementTime = Date.now() - timer.start - timer.responseTime;
+			console.log(timer.movementTime);
+			// write to database
+			console.log(timer)
+		}
+	})
+	/* 
+	When we start generating data from events, we'll want to work on writing information to the database.
+	We should only be WRITING information to the database -- no need to retrieve data right now on the application, since we can do that via the console. 
+	More information on https://firebase.google.com/docs/database/web/read-and-write
+	*/
+})
 function randomImg(){
     var randomNumber = Math.floor(Math.random() * 11) + 1;
     var imgName = randomNumber + ".png";
@@ -7,10 +47,3 @@ function randomImg(){
     var imgName2 = "City" + randomNumber2 + ".jpg";
     document.getElementById("bg").src= "./images/" + imgName2 ;
  }
-var database = firebase.database();
-
-/* 
-When we start generating data from events, we'll want to work on writing information to the database.
-We should only be WRITING information to the database -- no need to retrieve data right now on the application, since we can do that via the console. 
-More information on https://firebase.google.com/docs/database/web/read-and-write
-*/
