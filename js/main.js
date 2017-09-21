9292d445b6fb35743797e6d718f1267d1d2f237a
GRANT HOECHST <granthoechst@college.harvard.edu>
2017-09-21 01:35:35 -0400

    firebase functionality hooked up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-var timer =
+var trial =
 {
+	'sessionID': (Math.ceil(Math.random() * 900000000 + 100000000)),
+	// 0 for robot, 1 for human
+	'choice': "",
+	'face': 0,
+	'background': 0,
 	'start': 0, 
 	'responseTime': 0,
 	'movementTime': 0, 
@@ -7,31 +12,48 @@ var timer =
 	'stage2': false,
 	'coordinates': []
 }
-//var database = firebase.database();
+
+var database = firebase.database();
+
 var d = new Date();
 
+function writeUserData(input) {
+	 firebase.database().ref('users/' + input.sessionID + '/' + input.start).set({
+		choice: input.choice,
+		face: input.face,
+		background: input.background,
+		responseTime: input.responseTime,
+		movementTime: input.movementTime,
+		trajectory : input.coordinates
+	  	});
+	}
+
+
 $('document').ready(function(){
-d
+
 	$('#next').click(function(){
-		timer.stage1 = false, timer.stage2 = false;
-		timer.start = 0, timer.responseTime = 0, timer.movementTime = 0; 
-		timer.coordinates = []
-		timer.stage1 = true;
-		timer.start = Date.now(); console.log(timer.start);
+		trial.stage1 = false, trial.stage2 = false;
+		trial.start = 0, trial.responseTime = 0, trial.movementTime = 0; 
+		trial.coordinates = []
+		trial.stage1 = true;
+		trial.start = Date.now(); console.log(trial.start);
 	});
 	$("#next").mouseleave(function(){
-		if(!timer.stage1){
-			timer.stage1 = true;
-			timer.responseTime = Date.now() - timer.start; console.log(timer.responseTime)
+		if(!trial.stage1){
+			trial.stage1 = true;
+			trial.responseTime = Date.now() - trial.start; console.log(trial.responseTime)
 		}
 	});
 	$('.btn').mouseenter(function(){
-		if(!timer.stage2){
-			timer.stage2 = true;
-			timer.movementTime = Date.now() - timer.start - timer.responseTime;
-			console.log(timer.movementTime);
+		if(!trial.stage2){
+			trial.stage2 = true;
+			trial.movementTime = Date.now() - trial.start - trial.responseTime;
+			trial.choice = $(this).attr("id");
+			console.log(trial.movementTime);
+			
 			// write to database
-			console.log(timer)
+			writeUserData(trial);
+			console.log(trial)
 		}
 	})
 	/* 
@@ -42,10 +64,12 @@ d
 })
 function randomImg(){
     var randomNumber = Math.floor(Math.random() * 11) + 1;
+    trial.face = randomNumber ;
     var imgName = randomNumber + ".png";
     document.getElementById("face").src= "./images/" + imgName ;
 
     var randomNumber2 = Math.ceil(Math.random() * 3);
+    trial.background = randomNumber2 ;
     var imgName2 = "City" + randomNumber2 + ".jpg";
     document.getElementById("bg").src= "./images/" + imgName2 ;
  }
@@ -57,13 +81,13 @@ if(timer.stage1 && !timer.stage2 makes sure that we only record mouse movements 
 selection to only see the selection path and reduce noise.*/
 
  function handler(e) {
-	if(timer.stage1 && !timer.stage2){
+	if(trial.stage1 && !trial.stage2){
 		e = e || window.event;
 
 		var pageX = e.pageX;
 		var pageY = e.pageY;
 		console.log(pageX, pageY);
-		(timer.coordinates).push({x: pageX, y: pageY})
+		(trial.coordinates).push({x: pageX, y: pageY})
 	}
 }
 
